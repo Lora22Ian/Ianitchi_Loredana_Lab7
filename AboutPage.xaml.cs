a9d1a59733e6e956e8f3945eabac862cc26baa01
LOREDANA IANIȚCHI <loredana.ianitchi@stud.ubbcluj.ro>
2023-11-24 02:07:17 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ianitchi_Loredana_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ianitchi_Loredana_Lab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
