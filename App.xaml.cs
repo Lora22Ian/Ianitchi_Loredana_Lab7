299bb9a392e45186c60339d523e8463f3c769b26
LOREDANA IANIȚCHI <loredana.ianitchi@stud.ubbcluj.ro>
2023-11-28 19:39:25 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace Ianitchi_Loredana_Lab7
+﻿using System;
+using Ianitchi_Loredana_Lab7.Data;
+using System.IO;
+namespace Ianitchi_Loredana_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
